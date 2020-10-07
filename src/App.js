04bdfa421b880cd833e674d1166ececb904c45ac
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-10-07 09:43:19 +0100

    Initial commit with starting files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <body>
+        Our Pricing Annually Monthly Basic &dollar;19.99 &dollar;199.99 500 GB
+        Storage 2 Users Allowed Send up to 3 GB Learn More Professional
+        &dollar;24.99 &dollar;249.99 1 TB Storage 5 Users Allowed Send up to 10
+        GB Learn More Master &dollar;39.99 &dollar;399.99 2 TB Storage 10 Users
+        Allowed Send up to 20 GB Learn More
+        <div class="attribution">
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </body>
     </div>
   );
 }
