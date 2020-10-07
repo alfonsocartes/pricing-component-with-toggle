351cebe0a6ccc522933515a85835fae83c60b0a9
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-10-07 10:43:17 +0100

    Add initial stiling for pricing cards

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/pricing.css";
 
 export default function Pricing() {
   return (
@@ -12,43 +13,43 @@ export default function Pricing() {
         </div>
       </div>
       {/* Pricing cards */}
-      <div class="pricing-cards">
-        <div class="pricing-card-secondary">
+      <div>
+        <div class="pricing-card pricing-card-secondary">
           <ul class="price-list">
             <li class="price-list-header">Basic</li>
-            <li class="monthly-price">$19.99</li>
-            <li class="yearly-price">$199.99</li>
+            <li class="price monthly-price">$19.99</li>
+            <li class="price yearly-price">$199.99</li>
             <li>500 GB Storage</li>
             <li>2 Users Allowed</li>
             <li>Send up to 3 GB</li>
             <li>
-              <button>Learn More</button>
+              <button class="price-button">Learn More</button>
             </li>
           </ul>
         </div>
-        <div class="pricing-card-main">
+        <div class="pricing-card pricing-card-main">
           <ul class="price-list">
             <li class="price-list-header">Professional</li>
-            <li class="monthly-price">$24.99</li>
-            <li class="yearly-price">$249.99</li>
+            <li class="price monthly-price">$24.99</li>
+            <li class="price yearly-price">$249.99</li>
             <li>1 TB Storage</li>
             <li>5 Users Allowed</li>
             <li>Send up to 10 GB</li>
             <li>
-              <button>Learn More</button>
+              <button class="price-button">Learn More</button>
             </li>
           </ul>
         </div>
-        <div class="pricing-card-secondary">
+        <div class="pricing-card pricing-card-secondary">
           <ul class="price-list">
             <li class="price-list-header">Master</li>
-            <li class="monthly-price">$39.99</li>
-            <li class="yearly-price">$399.99</li>
+            <li class="price monthly-price">$39.99</li>
+            <li class="price yearly-price">$399.99</li>
             <li>2 TB Storage</li>
             <li>10 Users Allowed</li>
             <li>Send up to 20 GB</li>
             <li>
-              <button>Learn More</button>
+              <button class="price-button">Learn More</button>
             </li>
           </ul>
         </div>
